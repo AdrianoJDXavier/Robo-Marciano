9b1aeff4e6507e818a56185c69e9cd518554f021
Adriano Xavier <aj-dx@hotmail.com>
2022-02-12 19:36:57 -0300

    Adicionado funcionalidade de calculo

---
+++ app/src/main/java/com/example/robomarciano/activity_calc.java
@@ -0,0 +1,17 @@
+package com.example.robomarciano;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class avtivity_calc extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+
+}
